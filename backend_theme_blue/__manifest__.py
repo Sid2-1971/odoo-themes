467dd4d53c8e74c7d4b8979dbcad2fe52c06faf0
Sidnei Brianti <scbranti@gmail.com>
2018-04-20 18:07:33 -0300

    inicio

---
+++ backend_theme_blue/__manifest__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016, 2017 Openworx
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+
+{
+    "name": "Material/United Backend Theme - Blue",
+    "summary": "Odoo 10.0 community backend theme Blue",
+    "version": "10.0.1.0.23",
+    "category": "Themes/Backend",
+    "website": "http://www.exemplo.com",
+	"description": """
+		Basedo no modulo "Backend theme for Odoo 10.0 community edition".
+    """,
+	'images':[
+        'images/screen.png'
+	],
+    "author": "Sidnei Brianti",
+    "license": "LGPL-3",
+    "installable": True,
+    "depends": [
+	'web_responsive',
+    ],
+    "data": [
+        'views/assets.xml',
+        'views/res_company_view.xml',
+        'views/users.xml',
+        'views/sidebar.xml',
+    ],
+}
+
